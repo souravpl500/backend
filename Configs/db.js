bc713042d03b43fe393c87dbbde73c535ec3b200
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-02-25 10:41:45 +0530

    delete

--- Configs/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const URL = process.env.MongoURl
-
-const connection = mongoose.connect(URL);
-
-module.exports = {
-  connection,
-};
