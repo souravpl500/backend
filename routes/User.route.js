180fc3fd039a933a0510d86ba493c4d8543c057a
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-02-25 12:09:39 +0530

    Add files via upload

---
+++ routes/User.route.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { UserModel } = require("../models/User.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userRouter = express.Router();
+
+userRouter.post("/register", async (req, res) => {
+  const { name, email, password, gender } = req.body;
+  try {
+    bcrypt.hash(password, 5, async (err, secure_password) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const user = new UserModel({
+          name,
+          email,
+          password: secure_password,
+          gender,
+        });
+        await user.save();
+        res.send("Registered");
+      }
+    });
+  } catch (err) {
+    res.send("Error in registering the user");
+    console.log(err);
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.find({ email });
+    const hashed_password = user[0].password;
+    if (user.length > 0) {
+      bcrypt.compare(password, hashed_password, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ userID: user[0]._id }, "masai");
+          res.send({ msg: "Login Successfull", token: token });
+        } else {
+          res.send("Wrong Credntials");
+        }
+      });
+    } else {
+      res.send("Wrong Credntials");
+    }
+  } catch (err) {
+    res.send("Something went wrong");
+    console.log(err);
+  }
+});
+
+module.exports = {
+  userRouter,
+};
